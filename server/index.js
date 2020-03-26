b53147f6ffa856b1e06765bf1d33a07e2f2f63ab
Peerapat Teerawattanasook <p.teerawatt@gmail.com>
2020-03-26 15:24:39 -0700

    working with dynamic url

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 2985;
 
 app.use('/', express.static(__dirname + '/../client/dist'));
+
+app.get ('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname + '/../client/dist/index.html'));
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
